2f446c769db60d075bbc2c5984be9db39f71b854
Shekhar Molaj <shekharmolaj2044@gmail.com>
2026-07-01 22:32:13 +0530

    Initial Spring Boot setup for Report Service

---
+++ src/main/java/com/smartpay/smartpay_report_service/SmartpayReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartpay.smartpay_report_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpayReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartpayReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpay/smartpay_report_service/SmartpayReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpay.smartpay_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpayReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
